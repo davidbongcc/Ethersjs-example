eb5b2ea83183740800455bfb9aaba58d444f5521
jacobcreech <mrjacobcreech@gmail.com>
2019-04-29 16:20:23 -0500

    Updated initial modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';  
 import { NgModule } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WalletComponent } from './wallet/wallet.component';
 import { WalletDirective } from './wallet.directive';
@@ -17,12 +20,15 @@ import { WalletDirective } from './wallet.directive';
     WalletDirective
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     FlexLayoutModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    BrowserAnimationsModule
+    MatListModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
